f28aea53cea526f745dd0b1cff148154cfc99531
Jefersonrojas <heiser.rojas@upch.pe>
2024-11-20 18:50:31 -0500

    realizar cambios

---
+++ Practica 2/ejercicio_1.py
@@ -0,0 +1,7 @@
+usuario = input("Ingrese contraseña: ")
+contraseña= "Esthepancito"
+if usuario == contraseña:
+    print(f"contraseña coorecta, la contraseña correcta es {contraseña}.")
+else:
+    print("contraseña incorrecta")
+    
