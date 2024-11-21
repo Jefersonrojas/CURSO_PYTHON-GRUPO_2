6df647b6b20424bfc5bfa84af7a99a1daaa67810
AldairEduardo <ahuamanir@autonoma.edu.pe>
2024-11-20 19:31:02 -0500

    Se modificaron los archivos.

--- Practica 2/ejercicio_2.py
+++ Practica 2/ejercicio_2.py
@@ -1,3 +1,8 @@
+'''2. Para tributar un determinado impuesto se debe ser mayor de 16 años y tener unos
+ingresos iguales o superiores a 1000 € mensuales. Escribir un programa que pregunte
+al usuario su edad y sus ingresos mensuales y muestre por pantalla si el usuario tiene
+que tributar o no. (10p)'''
+
 edad= int(input("Cual es su edad? "))
 ingresos= float(input("cuales son sus ingresos mensuales?"))
 if edad >= 16  and ingresos >= 1000:
--- Practica 2/ejercicio_3.py
+++ Practica 2/ejercicio_3.py
@@ -1,10 +1,23 @@
+'''3. Los tramos impositivos para la declaración de la renta en un determinado país son los
+siguientes:
+
+Renta Tipo impositivo
+Menos de 10000€ 5%
+Entre 10000€ y 20000€ 15%
+Entre 20000€ y 35000€ 20%
+Entre 35000€ y 60000€ 30%
+Más de 60000€ 45%
+
+Escribir un programa que pregunte al usuario su renta anual y muestre por pantalla el
+tipo impositivo que le corresponde. (10p)'''
+
 renta= float(input("Cual es su renta anual: "))
 
 if renta>0 and renta <10000:
     print("5%")
-elif renta >=1000 and renta <= 2000:
+elif renta >=10000 and renta <= 20000:
     print("15%")
-elif renta > 2000 and renta <= 35000:
+elif renta > 20000 and renta <= 35000:
     print("20%")
 elif renta > 35000 and renta <= 60000:
     print("30%")
